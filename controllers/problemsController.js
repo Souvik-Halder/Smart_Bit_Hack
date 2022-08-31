0cf9a0da37be2bb9c7b26d1a024cb9279b0fc6b5
Ayesh Biswal <biswalayesh3@gmail.com>
2022-08-31 19:47:50 +0530

    Separate controller functions from routes

---
+++ controllers/problemsController.js
@@ -0,0 +1,72 @@
+const ProblemStatement = require('../models/ProblemStatements');
+
+// GET all problem statements
+const getProblems = async (req, res) => {
+  try {
+    const statements = await ProblemStatement.find();
+    res.json({ statements });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// GET form to add new problem
+const getNewProblemForm = (req, res) => {
+  res.render('addProblemForm', { title: 'Add New Problem' });
+};
+
+// GET one problem statement
+const getOneProblem = async (req, res) => {
+  res.json(res.problem);
+};
+
+// POST one problem statement
+const addProblem = async (req, res) => {
+  const problem = new ProblemStatement({
+    title: req.body.title,
+    description: req.body.description,
+    theme: req.body.theme,
+  });
+
+  try {
+    const newProblem = await problem.save();
+    res.status(201).json(newProblem);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// UPDATE one problem statement
+const updateProblem = async (req, res) => {
+  const currentCount = res.problem.submitCount;
+  res.problem.submitCount = currentCount + 1;
+
+  try {
+    const updatedProblem = await res.problem.save();
+    res.json(updatedProblem);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// DELETE a problem statement
+const deleteProblem = async (req, res) => {
+  try {
+    const problemId = res.problem._id;
+    await res.problem.remove();
+    res.json({
+      message: `Delete success for problem statement with id ${problemId}`,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getProblems,
+  getNewProblemForm,
+  getOneProblem,
+  addProblem,
+  updateProblem,
+  deleteProblem,
+};
